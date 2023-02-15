90033d755b6d3ee4f25729efba410ab00092c491
Charlotte1805 <109350847+Charlotte1805@users.noreply.github.com>
2023-02-15 17:32:41 +0100

    création dynamique des produits sur la page d'acceuil

--- front/js/script.js
+++ front/js/script.js
@@ -4,5 +4,21 @@ fetch("http://localhost:3000/api/products")
   .then((objetProduits) => {
 
 console.log(  objetProduits )
+
+let section_product = document.getElementById("items");
+    for (let i = 0; i < objetProduits.length; i++) {
+    let kanap = objetProduits[i];
+ 
+    //cree le lien balise a
+    let kanap_a = document.createElement("a");
+    kanap_a.href = "./product.html?id="+kanap._id;
+    //cree l'article
+    let kanap_article = document.createElement("article");
+   
+    //cree l'image
+    let kanap_img = document.createElement("img");
+    kanap_img.src = kanap.imageUrl;
+    kanap_img.alt = kanap.altTxt;
+ 
  
 })

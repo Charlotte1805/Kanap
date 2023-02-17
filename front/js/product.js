ecb986f06f8bd59d7f767bc279fd7dea74fea768
Charlotte1805 <109350847+Charlotte1805@users.noreply.github.com>
2023-02-17 18:32:36 +0100

    début de la page product

---
+++ front/js/product.js
@@ -0,0 +1,35 @@
+const 
+productId = new 
+URL(window.location.href).searchParams.get("id");
+
+
+
+console.log(
+productId );
+
+
+
+const 
+prodTitle = document.getElementById("title")
+
+const 
+prodColor = document.getElementById("colors")
+
+const 
+prodImage = document.querySelector(".item__img")
+
+const 
+prodDescription = document.getElementById("description")
+
+const 
+prodPrice = document.getElementById("price")
+
+const 
+prodColors = document.getElementById('colors')
+
+const 
+prodQuantity = document.getElementById('quantity')
+
+const 
+addToCartButton = document.getElementById('addToCart')
+

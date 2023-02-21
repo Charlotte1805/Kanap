d3a64936d38d9ba249e884120817b044eafd32c0
Charlotte1805 <109350847+Charlotte1805@users.noreply.github.com>
2023-02-21 18:00:20 +0100

    avancement sur la page product

--- front/js/product.js
+++ front/js/product.js
@@ -1,56 +1,83 @@
-const 
-productId = new 
-URL(window.location.href).searchParams.get("id");
+const productId = new URL(window.location.href).searchParams.get("id");
 
+console.log( productId );
 
+const prodTitle = document.getElementById("title")
 
-console.log(
-productId );
+const prodColor = document.getElementById("colors")
 
+const prodImage = document.querySelector(".item__img")
 
+const prodDescription = document.getElementById("description")
 
-const 
-prodTitle = document.getElementById("title")
+const prodPrice = document.getElementById("price")
 
-const 
-prodColor = document.getElementById("colors")
+const prodColors = document.getElementById('colors')
 
-const 
-prodImage = document.querySelector(".item__img")
+const prodQuantity = document.getElementById('quantity')
 
-const 
-prodDescription = document.getElementById("description")
-
-const 
-prodPrice = document.getElementById("price")
-
-const 
-prodColors = document.getElementById('colors')
-
-const 
-prodQuantity = document.getElementById('quantity')
-
-const 
-addToCartButton = document.getElementById('addToCart')
+const addToCartButton = document.getElementById('addToCart')
 
 
 fetch(`http://localhost:3000/api/products/${productId}`)
 
-    .then((res)
-=> 
-res.json())
+    .then((res) => res.json())
 
-    .then((object)
-=> {
+    .then((object) => {
 
-
-
-        console.log (
-object )
-
-
+        console.log ( object )
 
     })
+
+    .catch(function (err) {
+
+        console.log(err);
+
+    });
+
+fetch(`http://localhost:3000/api/products/${productId}`)
+
+    .then((res) => res.json())
+
+    .then((object) => {
+
+        for ( let color 
+
+        of object.colors ) { 
+            
+            let 
+  
+            optionColor = document.createElement('option')
+
+            optionColor.value = color
+
+            optionColor.innerText = color
+
+            prodColors.appendChild(optionColor)
+
+        }
+
+         let 
+     
+             productImg = document.createElement('img')
+
+             productImg.src = object.imageUrl
+
+             productImg.alt = object.altTxt
+
+             productImg.title = object.name
+
+             prodImage.appendChild(productImg)
+
+
+
+             prodTitle.innerText = object.name
+
+             prodPrice.innerText = object.price
+
+             prodDescription.innerText = object.description
+
+     })
 
     .catch(function (err) {
 

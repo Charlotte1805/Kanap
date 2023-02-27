e9cad30cf535012f617e9fa7d66da5d2d1acd4b0
Charlotte1805 <109350847+Charlotte1805@users.noreply.github.com>
2023-02-27 16:48:01 +0100

    constante+affichage des articles+lancement de l'affichage

--- front/js/cart.js
+++ front/js/cart.js
@@ -39,3 +39,93 @@ basket
     }
 
 }
+
+// Déclaration des constantes
+
+const cartItems = document.getElementById("cart__items")
+
+// Fonction showCartItems() qui permet d'affichage une ligne article dans le panier
+
+async 
+
+function showCartItems() {
+
+    // On récupère le contenu du panier
+
+    let basket = getBasket()
+
+    // Pour chaque produit
+
+    for ( let product of basket ) {   
+
+        // On appelle l'API pour récupérer les informations manquantes au localStorage
+
+        let productAPI = await 
+
+        fetch("http://localhost:3000/api/products/" + product.id)
+
+                .then((res) => res.json())
+
+                .then((product) => {
+
+                    return product
+
+                })
+
+                .catch(function (err) {
+
+                    console.log(err);
+
+                });
+
+        // On insère dybamiquement une ligne article dans le panier avec les informations du produit
+
+        cartItems.innerHTML += `<article class="cart__item" data-id="${productAPI._id}" data-color="${product.color}">
+
+            <div class="cart__item__img">
+
+              <img src= "${productAPI.imageUrl}" alt="${productAPI.altTxt}">
+
+            </div>
+
+            <div class="cart__item__content">
+
+              <div class="cart__item__content__description">
+
+                <h2>${productAPI.name}</h2>
+
+                <p>${product.color}</p>
+
+                <p>${productAPI.price}
+ 
+                €</p>
+
+              </div>
+
+              <div class="cart__item__content__settings">
+
+                <div class="cart__item__content__settings__quantity">
+
+                  <p>Qté : </p>
+
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+
+                </div>
+
+                <div class="cart__item__content__settings__delete">
+
+                  <p class="deleteItem">Supprimer</p>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </article>`;      
+
+    }
+
+};
+
+showCartItems()
